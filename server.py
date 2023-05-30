b9b199f8d8e2462e62fc7d83918bdcb38eaee4f5
YuryRass <superuser@mail.ru>
2023-05-30 19:10:38 +0300

    Refactoring code based on static analysis[D

--- server.py
+++ server.py
@@ -1,21 +1,24 @@
+""" TCP-server on sockets """
 import socket
 
-server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_sock.bind(('localhost', 5000))
+def server():
+    """ Creating TCP-server
+    """
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_sock.bind(('localhost', 5000))
 
-server_sock.listen()
-while True:
-    client_sock, client_addr = server_sock.accept()
-    print(f'Client {client_addr} connected...')
-    
+    server_sock.listen()
     while True:
-        data = client_sock.recv(4096)
-        
-        if not data:
-            break
-        else:
+        client_sock, client_addr = server_sock.accept()
+        print(f'Client {client_addr} connected...')
+        while True:
+            data = client_sock.recv(4096)
+            if not data:
+                break
             print(f"Recv data from client: {data.decode()}")
             client_sock.send("Hello!\n".encode())
-        
-    client_sock.close()
+        client_sock.close()
+    #Start the server with blocking functions:
+    server()
+    
